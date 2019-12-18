9028dcc2807a202ca4e079ca77d8d122788c044a
webii <webii@pm.me>
2019-12-18 09:18:56 +0400

    Get recent videos: return only videos

--- xdownload/pornhub.py
+++ xdownload/pornhub.py
@@ -4,7 +4,7 @@ import requests
 from pyquery import PyQuery as pq
 
 def login():
-    login_url = "https://www.tube8.com/signin.html"
+    login_url = ""
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
         }    
@@ -37,8 +37,9 @@ def get_recent_videos(ses, pages=[2, ], rus=False):
         parsed = pq(data)
         for el in parsed.items('li.videoBox a:first'):
             url = el.attr('href')
-            result.append("%s%s" % (domain, url))
-        time.sleep(10)
+            if "view_video" in href:
+                result.append("%s%s" % (domain, url))
+        time.sleep(10) # some user behavior emulation
     return result
 
 if __name__ == "__main__":
