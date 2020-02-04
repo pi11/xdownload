1ca1104423eb00ae4389176fa5dea44d9a321754
webii <webii@pm.me>
2020-02-04 15:44:37 +0400

    24video init

---
+++ xdownload/24video.py
@@ -0,0 +1,111 @@
+import re
+import time
+import requests
+from requests.exceptions import ConnectionError
+from pyquery import PyQuery as pq
+from urllib.parse import quote
+
+def login():
+    login_url = ""
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        }    
+    ses = requests.Session()
+    return ses
+
+def get_video_info(ses, url, tries=3, timeout=5):
+    retries = 1
+    data = False
+    while not data and retries <= tries:
+        try:
+            data = ses.get(url).text
+        except ConnectionError:
+            retries += 1
+            print("Connection error, sleeping for %s seconds" % (timeout * retries))
+            time.sleep(timeout * retries)
+
+    data = ses.get(url).text
+    parsed = pq(data)
+    title = parsed('h1.video-title:first').text()
+    tags = [c.text() for c in parsed('p.video-info-tags a').items()]
+    desc = parsed('p.desc').text()
+    return {"page":url, "title":title, "tags":tags, "description":desc}
+
+def parse_pornhub_url(ses, url, domain, DEBUG=False, tries=3, timeout=5):
+    result = []
+    retries = 1
+    data = False
+    while not data and retries <= tries:
+        try:
+            data = ses.get(url).text
+        except ConnectionError:
+            retries += 1
+            print("Connection error, sleeping for %s seconds" % (timeout * retries))
+            time.sleep(timeout * retries)
+        
+    parsed = pq(data)
+    for el in parsed.items('li.videoBox a:first'):
+        url = el.attr('href')
+        if "view_video" in url:
+            if DEBUG:
+                print('New video url found: %s' % url)
+            result.append("%s%s" % (domain, url))
+    return result
+
+def search_videos(ses, query, pages=[2, ], rus=False, DEBUG=False):
+    if rus:
+        domain = "https://rt.pornhub.com"
+        b_url = "https://rt.pornhub.com/video/search?search=%s&p=homemade&page=" % quote(query)
+    else:
+        b_url = "https://www.pornhub.com/video/search?search=%s&p=homemade&page=" % quote(query)
+        domain = "https://www.pornhub.com"
+    result = []
+    for p in pages:
+        url = b_url + str(p)
+        if DEBUG:
+            print('Loading: %s' % url)
+        new = parse_pornhub_url(ses, url, domain)
+        if new:
+            result += new
+        time.sleep(10) # some user behavior emulation
+    return result
+    
+    
+def get_recent_videos(ses, pages=[2, ], rus=False, DEBUG=False):
+    """ Function return dict with url, title and url for video download
+
+    Input: requests session,
+    list of pages to parse"""
+    
+    result = []
+    if rus:
+        domain = "https://rt.pornhub.com"
+        b_url = "https://rt.pornhub.com/video?p=homemade&o=mv&t=a&cc=us&hd=1&page="
+    else:
+        domain = "https://www.pornhub.com"
+        b_url = "https://www.pornhub.com/video?p=homemade&o=mv&cc=us&page="
+                      
+    for p in pages:
+        url = b_url + str(p)
+        if DEBUG:
+            print('Loading: %s' % url)
+        new = parse_pornhub_url(ses, url, domain)
+        if new:
+            result += new
+        time.sleep(10) # some user behavior emulation
+    return result
+
+if __name__ == "__main__":
+    ses = login()
+    v = get_video_info(ses, 'https://www.24video.vip/video/view/2706767')
+    print(v)
+    # example usage:
+    #for v in search_videos(ses, 'blowjob and anal', DEBUG=True)[:5]:
+    #    print("Video:", v)
+    #    print (get_video_info(ses, v))
+    #    time.sleep(5)
+
+    #for v in get_recent_videos(ses, DEBUG=True)[:5]:
+    #    print("Video:", v)
+    #    print (get_video_info(ses, v))
+    #    time.sleep(5)
