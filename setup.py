9b86d8676ce8d6869e2899efda4234ca3615775a
webii <webii@pm.me>
2020-03-27 06:07:15 +0400

    ver bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xdownload',
-      version='0.0.7',
+      version='0.0.8',
       description='Video downloader/meta tag extractor for various web sites',
       url='https://bitbucket.org/pi11/xdownload',
       packages=['xdownload',],
