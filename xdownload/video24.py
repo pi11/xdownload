d95e4faa43ac6e4a8c67b7948948ba057a6302b1
Brian <brian.r0dev@gmail.com>
2021-03-10 23:32:01 +0400

    24video fix

--- xdownload/video24.py
+++ xdownload/video24.py
@@ -5,7 +5,7 @@ from requests.exceptions import ConnectionError
 from pyquery import PyQuery as pq
 from urllib.parse import quote
 
-_DOMAIN = "https://vip.24video.click/"
+_DOMAIN = "https://vip.24video.click"
 
 def login(proxies={}):
     login_url = ""
